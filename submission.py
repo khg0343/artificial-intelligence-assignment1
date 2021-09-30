fc55a58e71e2239b15fa2e105396c3b32a179ca9
khg0343 <sally0343@gmail.com>
2021-09-30 23:11:27 +0900

    I found Source code
    from https://magician-of-c.tistory.com/

--- submission.py
+++ submission.py
@@ -1,4 +1,4 @@
-## ID: 20212656 NAME: Hwang Seonjeong
+## ID: 20180373 NAME: Kim Hyeonji
 ######################################################################################
 # Problem 2a
 # minimax value of the root node: 100
@@ -135,7 +135,7 @@ class MinimaxAgent(MultiAgentSearchAgent):
   """
     Your minimax agent (problem 1)
   """
-
+  
   def getAction(self, gameState):
     """
       Returns the minimax action from the current gameState using self.depth
@@ -172,7 +172,48 @@ class MinimaxAgent(MultiAgentSearchAgent):
     """
 
     # BEGIN_YOUR_ANSWER (our solution is 30 lines of code, but don't worry if you deviate from this)
-    raise NotImplementedError  # remove this line before writing code
+    # raise NotImplementedError  # remove this line before writing code
+    def maximizer(state, depth):
+          if depth == self.depth or state.isWin() or state.isLose():
+                return self.evaluationFunction(state)
+
+          value = float("-inf")
+          legalMoves = state.getLegalActions()
+
+          for action in legalMoves:
+                value = max(value, minimizer(state.generateSuccessor(0, action), depth, 1))
+
+          return value
+
+    def minimizer(state, depth, agentIndex):
+          if depth == self.depth or state.isWin() or state.isLose():
+                return self.evaluationFunction(state)
+
+          value = float("-inf")
+          legalMoves = state.getLegalActions(agentIndex)
+
+          if agentIndex == state.getNumAgents()-1:
+                for action in legalMoves:
+                      value = min(value, maximizer(state.generateSuccessor(agentIndex, action), depth+1))
+          else :
+                for action in legalMoves:
+                      value = min(value, minimizer(state.generateSuccessor(agentIndex, action), depth, agentIndex+1))
+                
+          return value
+
+    legalMoves = gameState.getLegalActions()
+    move = Directions.STOP
+    value = float("-inf")
+
+    for action in legalMoves:
+          temp = minimizer(gameState.generateSuccessor(0, action), 0, 1)
+          
+          if temp > value :
+                value = temp
+                move = action
+
+    return move
+          
     # END_YOUR_ANSWER
 
 ######################################################################################
@@ -189,7 +230,60 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
     """
 
     # BEGIN_YOUR_ANSWER (our solution is 42 lines of code, but don't worry if you deviate from this)
-    raise NotImplementedError  # remove this line before writing code
+    # raise NotImplementedError  # remove this line before writing code
+    def maximizer(state, depth, alpha, beta):
+          if depth == self.depth or state.isWin() or state.isLose():
+                return self.evaluationFunction(state)
+
+          value = float("-inf")
+          legalMoves = state.getLegalActions()
+
+          for action in legalMoves:
+                value = max(value, minimizer(state.generateSuccessor(0, action), depth, 1, alpha, beta))
+                if value > beta :
+                      return value
+                alpha = max(alpha, value)
+          
+          return value
+
+    def minimizer(state, depth, agentIndex, alpha, beta):
+          if depth == self.depth or state.isWin() or state.isLose():
+                return self.evaluationFunction(state)
+
+          value = float("-inf")
+          legalMoves = state.getLegalActions(agentIndex)
+
+          if agentIndex == state.getNumAgents()-1:
+                for action in legalMoves:
+                      value = min(value, maximizer(state.generateSuccessor(agentIndex, action), depth+1, alpha, beta))
+                      if value < alpha:
+                            return value
+                      beta = min(beta, value)
+          else :
+                for action in legalMoves:
+                      value = min(value, minimizer(state.generateSuccessor(agentIndex, action), depth, agentIndex+1, alpha, beta))
+                      if value < alpha:
+                            return value
+                      beta = min(beta, value)
+                
+          return value
+
+    legalMoves = gameState.getLegalActions()
+    move = Directions.STOP
+    value = float("-inf")
+    alpha = float("-inf")
+    beta = float("-inf")
+
+    for action in legalMoves:
+          temp = minimizer(gameState.generateSuccessor(0, action), 0, 1, alpha, beta)
+          
+          if temp > value :
+                value = temp
+                move = action
+          
+          alpha = max(alpha, value)
+
+    return move
     # END_YOUR_ANSWER
 
 ######################################################################################
@@ -209,7 +303,47 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
     """
 
     # BEGIN_YOUR_ANSWER (our solution is 30 lines of code, but don't worry if you deviate from this)
-    raise NotImplementedError  # remove this line before writing code
+    # raise NotImplementedError  # remove this line before writing code
+    def maximizer(state, depth):
+          if depth == self.depth or state.isWin() or state.isLose():
+                return self.evaluationFunction(state)
+
+          value = float("-inf")
+          legalMoves = state.getLegalActions()
+
+          for action in legalMoves:
+                value = max(value, expecter(state.generateSuccessor(0, action), depth, 1))
+
+          return value
+
+    def expecter(state, depth, agentIndex):
+          if depth == self.depth or state.isWin() or state.isLose():
+                return self.evaluationFunction(state)
+
+          value = 0
+          legalMoves = state.getLegalActions(agentIndex)
+
+          if agentIndex == state.getNumAgents()-1:
+                for action in legalMoves:
+                      value += maximizer(state.generateSuccessor(agentIndex, action), depth+1)
+          else :
+                for action in legalMoves:
+                      value += expecter(state.generateSuccessor(agentIndex, action), depth, agentIndex+1)
+                
+          return value/len(legalMoves)
+
+    legalMoves = gameState.getLegalActions()
+    move = Directions.STOP
+    value = float("-inf")
+
+    for action in legalMoves:
+          temp = expecter(gameState.generateSuccessor(0, action), 0, 1)
+          
+          if temp > value :
+                value = temp
+                move = action
+
+    return move
     # END_YOUR_ANSWER
 
 ######################################################################################
@@ -221,7 +355,46 @@ def betterEvaluationFunction(currentGameState):
   """
 
   # BEGIN_YOUR_ANSWER (our solution is 60 lines of code, but don't worry if you deviate from this)
-  raise NotImplementedError  # remove this line before writing code
+  # raise NotImplementedError  # remove this line before writing code
+  currentPos = currentGameState.getPacmanPosition()
+  currentFood = currentGameState.getFood()
+  capsulePos = currentGameState.getCapsules()
+  layout = currentGameState.getWalls()
+
+  maxlength = layout.height - 2 + layout.width - 2
+  fooddistance = []
+  capsuledistance = []
+
+  for food in currentFood.asList():
+        fooddistance.append(manhattanDistance(currentPos, food))
+
+  for capsule in capsulePos:
+        capsuledistance.append(manhattanDistance(currentPos, capsule))
+
+  score = 0
+  x = currentPos[0]
+  y = currentPos[1]
+
+  for ghostState in currentGameState.getGhostStates():
+        gd = manhattanDistance(currentPos, ghostState.configuration.getPosition())
+
+        if gd < 2:
+              if ghostState.scaredTimer != 0:
+                    score += 1000.0/(gd+1)
+              else : 
+                    score -= 1000.0/(gd+1)
+  
+  if min(capsuledistance+[float(100)])<5:
+        score += 500.0/(min(capsuledistance))
+
+  for capsule in capsulePos:
+        if (capsule[0]==x) & (capsule[1]==y):
+              score += 600.0
+
+  minfooddistance = min(fooddistance+[float(100)])
+
+  return score + 1.0/minfooddistance - len(fooddistance)*10.0 
+
   # END_YOUR_ANSWER
 
 # Abbreviation
